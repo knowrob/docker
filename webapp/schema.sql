12acda9202918d50c3096d132877ad8f28c36464
Moritz Tenorth <tenorth@cs.uni-bremen.de>
2014-07-01 09:38:16 +0200

    updated DB schema: include table for tutorial texts

--- webapp/schema.sql
+++ webapp/schema.sql
@@ -1,6 +1,8 @@
-drop table if exists entries;
-create table entries (
+drop table if exists tutorial;
+create table tutorial (
 	id integer primary key autoincrement,
+	cat_id text not null,	
+	cat_title text not null,
 	title text not null,
 	text text not null
 );
@@ -8,7 +10,7 @@ create table entries (
 drop table if exists users;
 create table users (
         id integer primary key autoincrement,
-        username text not null,
+        username text unique not null,
         email text not null,
         passwd text not null,
         container_id text not null
