b10c5f59adcaf314a8ef4e9f9a1451d5136dd736
Moritz Tenorth <tenorth@cs.uni-bremen.de>
2014-11-12 10:00:20 +0100

    renamed folders in knowrob/docker to match the container identifiers used

---
+++ postgres/setup-schema.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "******CREATING DOCKER DATABASE******"
+gosu postgres postgres --single <<- EOSQL
+   CREATE DATABASE docker;
+   CREATE USER docker;
+   GRANT ALL PRIVILEGES ON DATABASE docker to docker;
+EOSQL
+echo ""
+echo "******DOCKER DATABASE CREATED******
