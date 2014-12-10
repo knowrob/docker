c0b341f83f1ee8b679f175406e9848be6726805d
Daniel Beßler <daniel@orgizm.net>
2014-12-10 21:13:03 +0100

    Fix for problems with postgres configuration where the user 'docker' is not known to the postgres server. Fix is to set env var POSTGRES_USER for postgres container.

--- postgres/setup-schema.sh
+++ postgres/setup-schema.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo "******CREATING DOCKER DATABASE******"
 gosu postgres postgres --single <<- EOSQL
-   CREATE DATABASE docker;
-   CREATE USER docker;
    GRANT ALL PRIVILEGES ON DATABASE docker to docker;
 EOSQL
+#gosu postgres postgres --single <<- EOSQL
+#   CREATE DATABASE docker;
+#   CREATE USER docker;
+#   GRANT ALL PRIVILEGES ON DATABASE docker to docker;
+#EOSQL
 echo ""
 echo "******DOCKER DATABASE CREATED******
