9e3b4e85167162cae636fd82e7469eeb425435ed
Daniel Beßler <daniel@orgizm.net>
2014-12-15 21:36:04 +0100

    More usable build script that does not rely on ROS installation. If no knowrob installation can be found it is automatically cloned.

--- hydro-knowrob-daemon-local/build.sh
+++ hydro-knowrob-daemon-local/build.sh
@@ -1,17 +1,88 @@
 #!/bin/sh
 # Author: Daniel Beßler
 
-DIR=`pwd`
+NAMES="knowrob knowrob_addons knowrob_dev iai_maps iai_common_msgs iai_cad_tools"
 
-WS=`echo $ROS_PACKAGE_PATH | tr ":" "\n" | head -n 1`
-echo "Using catkin workspace at: $WS";
+SCRIPT=`readlink -f "$0"`
+DIR=`dirname $SCRIPT`
+LOCAL_DIR="$DIR/knowrob"
+KNOWROB_FOUND=0
 
-echo "Creating ´src´ archive....";
-cd $WS
-rm -rf ./src.tar
-tar --exclude=.svn --exclude=.git --exclude=build --exclude=.gradle -cf ./src.tar ./*
-cd $DIR
-mv ${WS}/src.tar .
+is_knowrob_dir() {
+  if [ ! -d "$1" ]; then
+    return 1
+  fi
+  echo "Checking directory $1 ..."
+  VALID=0
+  for n in $NAMES; do
+    if ls $1 | grep ^$n$ > /dev/null; then
+      echo "    $n found."
+    else
+      echo "    $n not found."
+      VALID=1
+    fi
+  done
+  return $VALID
+}
+
+create_archive() {
+  cd $1
+  rm -rf ./src.tar
+  tar --exclude=.svn --exclude=.git --exclude=build --exclude=.gradle -cf ./src.tar ./*
+  mv ./src.tar $DIR/
+  cd $DIR
+}
+
+clone_knowrob_repo() {
+  cd $LOCAL_DIR
+  if ls $LOCAL_DIR | grep ^$1$ > /dev/null; then
+    echo "$1 already cloned."
+  else
+    git clone --recursive $2 -b $3
+  fi
+  cd $DIR
+}
+
+clone_knowrob() {
+  clone_knowrob_repo knowrob         https://github.com/knowrob/knowrob.git           indigo-devel
+  clone_knowrob_repo knowrob_addons  https://github.com/knowrob/knowrob_addons.git    indigo-devel
+  clone_knowrob_repo knowrob_dev     https://github.com/code-iai/knowrob_dev.git      master
+  clone_knowrob_repo iai_maps        https://github.com/code-iai/iai_maps.git         master
+  clone_knowrob_repo iai_common_msgs https://github.com/code-iai/iai_common_msgs.git  master
+  clone_knowrob_repo iai_cad_tools   https://github.com/code-iai/iai_cad_tools.git    master
+}
+
+echo "Script is executed at: $DIR"
+
+# Try local directory first
+is_knowrob_dir $LOCAL_DIR
+if [ $? -eq 0 ]; then
+  create_archive $LOCAL_DIR
+  KNOWROB_FOUND=1
+fi
+
+# Try ROS_PACKAGE_PATH head directory
+if [ $KNOWROB_FOUND -eq 0 ]; then
+  if env | grep -q ^ROS_PACKAGE_PATH=; then
+    echo "ROS_PACKAGE_PATH is defined."
+    WS=`echo $ROS_PACKAGE_PATH | tr ":" "\n" | head -n 1`
+    is_knowrob_dir $WS
+    if [ $? -eq 0 ]; then
+      create_archive $WS
+      KNOWROB_FOUND=1
+    fi
+  else
+    echo "WARNING: ROS_PACKAGE_PATH is not defined."
+  fi
+fi
+
+# Checkout knowrob locally
+if [ $KNOWROB_FOUND -eq 0 ]; then
+  echo "Unable to locate knowrob installation. Checking it out via git..."
+  clone_knowrob
+  create_archive $LOCAL_DIR
+fi
 
 echo "Building knowrob/hydro-knowrob-daemon....";
 docker build -t knowrob/hydro-knowrob-daemon .
+
