c720ac0afc1feddce0223fcc4131db6ca0d040bf
Mareike <mareikep@tzi.de>
2015-04-15 09:15:19 +0200

    uncomment loading schema and tutorials as it is not needed yet

--- postgres/setup-schema.sh
+++ postgres/setup-schema.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-echo "******CREATING DOCKER DATABASE******"
-gosu postgres postgres --single <<- EOSQL
-    CREATE USER docker;
-    CREATE DATABASE docker;
-EOSQL
+# echo "******CREATING DOCKER DATABASE******"
+# gosu postgres postgres --single <<- EOSQL
+#     CREATE USER docker;
+#     CREATE DATABASE docker;
+# EOSQL
 
-echo "Loading schema and tutorials..."
-gosu postgres pg_ctl -w start
-gosu postgres psql -d docker -f /tmp/schema.sql
-gosu postgres psql -d docker -f /tmp/tutorial.sql
-gosu postgres pg_dump docker -f /tmp/backup.dump
-echo "Successful. Stopping postgres..."
-gosu postgres pg_ctl -w stop
-echo "Done."
+# echo "Loading schema and tutorials..."
+# gosu postgres pg_ctl -w start
+# gosu postgres psql -d docker -f /tmp/schema.sql
+# gosu postgres psql -d docker -f /tmp/tutorial.sql
+# gosu postgres pg_dump docker -f /tmp/backup.dump
+# echo "Successful. Stopping postgres..."
+# gosu postgres pg_ctl -w stop
+# echo "Done."
 
 echo "******GRANTING PRIVILEGES******"
 gosu postgres postgres --single <<- EOSQL
