e4cff48b2156b46802235ed4d55e4856661b60d0
Mareike <mareikep@tzi.de>
2015-05-27 18:45:51 +0200

    rename tutorial model, fix setup-schema

--- postgres/setup-schema.sh
+++ postgres/setup-schema.sh
@@ -6,15 +6,13 @@
 echo "******CREATING DOCKER AND TUTORIAL DATABASE******"
 gosu postgres postgres --single <<- EOSQL
   CREATE DATABASE docker;
-  CREATE DATABASE tutorial;
   CREATE USER docker;
   GRANT ALL PRIVILEGES ON DATABASE docker to docker;
-  GRANT ALL PRIVILEGES ON DATABASE tutorial to docker;
 EOSQL
 
 echo "******WRITING SQL DUMP TO DATABASE TUTORIAL******"
 gosu postgres pg_ctl -w start
-gosu postgres psql -d tutorial -f /tmp/schema.sql
-gosu postgres psql -d tutorial -f /tmp/tutorial.sql
+gosu postgres psql -d docker -f /tmp/schema.sql
+gosu postgres psql -d docker -f /tmp/tutorial.sql
 gosu postgres pg_ctl stop
 echo "******TUTORIAL CONTENT WRITTEN******"
