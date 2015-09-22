4ce541e96ed0cb0e85327029e179696422797d17
Daniel Beßler <danielb@informatik.uni-bremen.de>
2015-09-22 12:54:02 +0200

    cleanup

--- flask/webrob/config/settings.py
+++ flask/webrob/config/settings.py
@@ -10,10 +10,10 @@ SQLALCHEMY_DATABASE_URI = 'postgresql://docker@' + \
 CSRF_ENABLED = True
 
 # email server
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 465
-MAIL_USE_TLS = False
-MAIL_USE_SSL = True
+MAIL_SERVER   = os.environ.get('OPENEASE_MAIL_SERVER', 'smtp.gmail.com')
+MAIL_PORT     = int(os.environ.get('OPENEASE_MAIL_PORT', '465'))
+MAIL_USE_TLS  = bool(os.environ.get('OPENEASE_MAIL_USE_TLS', 'False'))
+MAIL_USE_SSL  = bool(os.environ.get('OPENEASE_MAIL_USE_SSL', 'True'))
 MAIL_USERNAME = os.environ.get('OPENEASE_MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('OPENEASE_MAIL_PASSWORD')
 MAIL_DEFAULT_SENDER = '"Sender" <openease.iai@gmail.com>'
