699d8f2faec3d22c2bff58098cf027a90a690830
Daniel Beßler <danielb@cs.uni-bremen.de>
2016-12-13 12:18:22 +0100

    clean up

--- hydro-swi/build.sh
+++ hydro-swi/build.sh
@@ -5,6 +5,7 @@ SCRIPT=`readlink -f "$0"`
 DIR=`dirname $SCRIPT`
 
 $DIR/../scripts/start-apt-cacher
+$DIR/../scripts/start-nexus
 echo "Building openease/hydro-swi....";
 docker build -t openease/hydro-swi .
 
