fcf987db29bf98844e7ac85eacf95dcd9acd6c48
Daniel Beßler <danielb@cs.uni-bremen.de>
2016-12-13 15:23:56 +0100

    rearranged directory structure of different knowrob containers

--- indigo/indigo-swi/build.sh
+++ indigo/indigo-swi/build.sh
@@ -6,6 +6,6 @@ DIR=`dirname $SCRIPT`
 
 $DIR/../scripts/start-apt-cacher
 $DIR/../scripts/start-nexus
-echo "Building openease/hydro-swi....";
-docker build -t openease/hydro-swi .
+echo "Building openease/indigo-swi....";
+docker build "$@" -t openease/indigo-swi .
 
